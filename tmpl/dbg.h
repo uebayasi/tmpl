400e0243bcad236fe44bad3accae1e5ba5051b46
Masao Uebayashi <uebayasi@tombiinc.com>
2015-10-24 06:12:54 +0900

    Split debug print.

---
+++ tmpl/dbg.h
@@ -0,0 +1,34 @@
+/* dbg */
+
+#if 0
+#define	DBG(...)	do {} while (0)
+#define	DBGINDENT()	do {} while (0)
+#define	DUMPCHAR()	do {} while (0)
+#define	DUMPBUF()	do {} while (0)
+#else
+#define	DEBUG
+#define	DBGINDENT()	do { \
+	int d = stack.depth; \
+	while (d++ < MACRO_DEPTH - 1) fputc('\t', stderr); \
+} while (0)
+#define	DBG(...)	do { \
+	DBGINDENT(); \
+	fprintf(stderr, __VA_ARGS__); \
+} while (0)
+#define	PC(c)		((vc(c) == 0) ? '?' : vc(c))
+#define	DUMPCHAR(l, c, n)	do { \
+	if ((c) < 0x20) \
+		fprintf(stderr, "%c%c%c%c%s", (l), '\\', PC(c), (l) + 2, (n)); \
+	else \
+		fprintf(stderr, "%c%c%c%c%s", (l), ' ', c, (l) + 2, (n)); \
+} while (0)
+#define	DUMPBUF()	do { \
+	char *str; \
+	fprintf(stderr, "===|"); \
+	for (str = sb->head; str != sb->tail; str++) { \
+		char c = *str; \
+		DUMPCHAR('[', c, ""); \
+	} \
+	fprintf(stderr, "|===\n"); \
+} while (0)
+#endif
